bcfa96b13a6181b94496707d9161cd87306dc442
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-09-25 19:40:51 +0200

    Rename the plugin

--- src/main/java/me/ionar/salhack/SalHackMod.java
+++ src/main/java/me/ionar/salhack/SalHackMod.java
@@ -12,7 +12,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "salhack", name = "SalHack", version = SalHackMod.VERSION)
+@Mod(modid = "pistonhack", name = "PistonHack", version = SalHackMod.VERSION)
 public final class SalHackMod
 {
     public static final String NAME = "SalHack";
